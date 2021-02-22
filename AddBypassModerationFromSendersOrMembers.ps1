8ab5b21c3ebe38b2c884f236d4152589fbf8d85a
Дмитрий Карпунин <dkarpunin@users.noreply.github.com>
2021-02-22 22:37:50 +0500

    Add files via upload

---
+++ AddBypassModerationFromSendersOrMembers.ps1
@@ -0,0 +1,11 @@
+﻿$Groups = "Group1", "Group2", "Group3"
+
+foreach ($Group in $Groups) {$Moder = (Get-DistributionGroup $Group).BypassModerationFromSendersOrMembers.Name
+
+	$User = "User"
+
+	if ($Moder.Count -gt 1) {$Moder += $User} else {$Moder = @($Moder, $User)}
+
+	Set-DistributionGroup $Group -BypassModerationFromSendersOrMembers $Moder
+
+}
